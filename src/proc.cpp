b035d522a01e551da6fefdd73be012925a0d4a85
TomaszPluta <tomasz.pluta.g2@gmail.com>
2019-10-23 21:37:32 +0200

    works fine

--- src/proc.cpp
+++ src/proc.cpp
@@ -24,88 +24,6 @@ using namespace std::string_literals;
 
 
 
-bool IsNumber(std::string num){
-	for (const auto & i : num){
-		if (!(isdigit(i))){
-			return false;
-		}
-	}
-	return true;
-}
-
-
-bool IsNumber2(std::string num){
-	return (std::find_if(num.begin(), num.end(), [&](auto i){return (!isdigit(i));}) ==  std::end(num));
-}
-
-
-
-
-std::vector<fs::path> GetSubDirectories(fs::path dir){
-	std::vector<fs::path> subDirs;
-	if ((fs::exists(dir)) && (fs::is_directory(dir))){
-		for (const auto & entry : fs::directory_iterator(dir)){
-			if (fs::is_directory(entry.status())){
-				subDirs.push_back(entry);
-				std::cout<<entry.path()<<std::endl;
-
-				auto it = entry.path().end();
-				it--; //w jednej lini?
-
-				std::cout<<*it<<std::endl;
-				if (IsNumber2(std::string(*it))){
-					std::cout<<"found "<<entry.path().c_str()<<std::endl;
-				} else{
-					std::cout<<"NOT found: "<<entry.path().c_str()<<std::endl;
-				}
-				std::cout<<std::endl;
-			}
-		}
-	}
-	return subDirs;
-}
-
-
-std::map<std::string, std::string>  GetKeyValuesFromFile(fs::path filePath){
-	std::map<std::string, std::string> keyVals;
-	std::ifstream sfile(filePath);
-	if (sfile.is_open()){
-		std::string line;
-		while (std::getline(sfile, line)){
-			std::stringstream streamLine(line);
-			std::string key;
-			while(std::getline(streamLine, key, ':')){
-				std::string value;
-				if (std::getline(streamLine, value)){
-					keyVals[key]= value;
-				}
-			}
-
-		}
-	}
-	return keyVals;
-//z https://stackoverflow.com/questions/6892754/creating-a-simple-configuration-file-and-parser-in-c/6892829#6892829
-}
-
-
-
-std::vector<fs::path> GetFilesInDir(fs::path dir){
-	std::vector<fs::path> files;
-	if ((fs::exists(dir)) && (fs::is_directory(dir))){
-		for (const auto & entry : fs::directory_iterator(dir)){
-			if (fs::is_regular_file(entry.status())){
-				files.push_back(entry);
-				GetKeyValuesFromFile(entry);
-			}
-		}
-	}
-	return files;
-}
-
-
-
-
-
 
 class ProcessInfo{
 	std::string _name{""};
@@ -134,7 +52,7 @@ public:
 		_pid = threads;
 		return *this;
 	}
-	ProcessInfo &comandLine(std::string name){
+	ProcessInfo &addComandLine(std::string name){
 		_comandLine = name;
 		return *this;
 	}
@@ -143,10 +61,95 @@ public:
 
 
 
+
+bool IsNumber(std::string num){
+	return (std::find_if(num.begin(), num.end(), [&](auto i){return (!isdigit(i));}) ==  std::end(num));
+}
+
+
+//std::string GetFileName
+
+
+
+
+std::vector<fs::path> GetSubDirectories(fs::path dir){
+	std::vector<fs::path> subDirs;
+	if ((fs::exists(dir)) && (fs::is_directory(dir))){
+		for (const auto & entry : fs::directory_iterator(dir)){
+			if (fs::is_directory(entry.status())){
+
+				std::cout<<entry.path()<<std::endl;
+
+				auto it = entry.path().end();
+				it--;
+
+				std::cout<<*it<<std::endl;
+				if (IsNumber(std::string(*it))){
+					std::cout<<"found "<<entry.path().c_str()<<std::endl;
+					subDirs.push_back(entry); ////przerobic na zwenetrzna funkcje
+				} else{
+					std::cout<<"NOT found: "<<entry.path().c_str()<<std::endl;
+				}
+				std::cout<<std::endl;
+			}
+		}
+	}
+	return subDirs;
+}
+
+
+
+
+std::map<std::string, std::string>  GetKeyValuesFromFile(fs::path filePath){
+	std::map<std::string, std::string> keyVals;
+	std::ifstream sfile(filePath);
+	if (sfile.is_open()){
+		std::string line;
+		while (std::getline(sfile, line)){
+			std::stringstream streamLine(line);
+			std::string key;
+			while(std::getline(streamLine, key, ':')){
+				std::string value;
+				if (std::getline(streamLine, value)){
+					keyVals[key]= value;
+				}
+			}
+
+		}
+	}
+	return keyVals;
+}
+
+
+
+std::vector<fs::path> GetFilesInDir(fs::path dir){
+	std::vector<fs::path> files;
+	if ((fs::exists(dir)) && (fs::is_directory(dir))){
+		for (const auto & entry : fs::directory_iterator(dir)){
+			if (fs::is_regular_file(entry.status())){
+				files.push_back(entry); ///
+				if (entry.path().filename() == "status"){
+					std::map<std::string, std::string> stats = GetKeyValuesFromFile(entry);
+					ProcessInfo pInfo;
+					pInfo.addName(std::string(stats["Name"])).addPid(std::stoi(std::string(stats["Pid"])));
+					asm volatile ("nop");
+				}
+			}
+		}
+	}
+	return files;
+}
+
+
+
+
+
+
 class Processes{
 	void Traverse(){
 
 	}
+
 public:
 	Processes() = default;
 
@@ -157,7 +160,7 @@ public:
 int main(int argc, char **argv)
 {
 	std::vector<fs::path> subDirs = GetSubDirectories("/proc");
-	std::vector<fs::path> files = GetFilesInDir("/proc/1487");
+	std::vector<fs::path> files = GetFilesInDir(subDirs[0]);
 //	std::cout<<procFiles;
 	std::cin.get();
 }
