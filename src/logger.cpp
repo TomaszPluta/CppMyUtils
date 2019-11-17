535bc0995d7ce3ac5c0c25ed02b09b658e7b0718
TomaszPluta <tomasz.pluta.g2@gmail.com>
2019-11-17 21:58:55 +0100

    logger composit factory works

--- src/logger.cpp
+++ src/logger.cpp
@@ -4,32 +4,81 @@
 #include "string_view"
 #include "fstream"
 #include "string_view"
-#include  "sstream"
+#include "sstream"
 #include "memory"
+#include "vector"
+
+//dodac chrono
 
 
 class Logger{
 public :
-	virtual void writeInfo(std::string_view info) =0;
-	virtual void writeWarn(std::string_view warn) =0;
-	virtual void writeError(std::string_view error) =0;
+	virtual void writeInfo(const std::string_view info) =0;
+	virtual void writeWarn(const std::string_view warn) =0;
+	virtual void writeError(const std::string_view error) =0;
 };
 
 
-class FilesystemLogger : public Logger{
-	void writeInfo(std::string_view info){
-		std::cout<<info<<'\n';
+
+class CompositLogger : public Logger{
+	std::vector<std::shared_ptr<Logger>> loggers;
+
+public :
+	void addLogger(std::shared_ptr<Logger> logger){
+		loggers.push_back(logger);
 	}
-	void writeWarn(std::string_view warn){
-		std::cout<<warn<<'\n';
+
+	void writeInfo(const std::string_view info) override{
+		for (auto &i : loggers){
+			i->writeInfo(info);
+		}
 	}
-	void writeError(std::string_view error){
-		std::cout<<error<<'\n';
+
+	void writeWarn(const std::string_view warn) override{
+		for (auto &i : loggers){
+			i->writeWarn(warn);
+		}
 	}
+
+	void writeError(const std::string_view error) override{
+		for (auto &i : loggers){
+			i->writeError(error);
+		}
+	}
+
+
+};
+
+
+
+class FileLogger : public Logger{
+	std::ofstream loggerFile;
+
+public:
+	void writeInfo(const std::string_view info){
+		loggerFile<<"[info]"<<info<<'\n';
+	}
+	void writeWarn(const std::string_view warn){
+		loggerFile<<"[warn]"<<warn<<'\n';
+	}
+	void writeError(const std::string_view error){
+		loggerFile<<"[error]"<<error<<'\n';
+	}
+
+	FileLogger(){
+		loggerFile.open("logFile1.log", std::ofstream::app);
+	}
+
+	~FileLogger(){
+		loggerFile<<"[endSession]\n\n";
+	}
+
 };
 
 
 class StdOutLogger : public Logger{
+
+public:
 	void writeInfo(std::string_view info){
 			std::cout<<info<<'\n';
 		}
@@ -42,72 +91,209 @@ class StdOutLogger : public Logger{
 };
 
 
-class LoggerFactory{
-	std::string configFileName;
-	 enum class logginOutput : int{
-		 FILE,
-		 SOUT
-	 };
+
+class RemoteLogger : public Logger{
 
 public:
-	 LoggerFactory(std::string_view cfgFileName) : configFileName{cfgFileName}{};
+	void writeInfo(std::string_view info){
+			std::cout<<info<<'\n';
+		}
+		void writeWarn(std::string_view warn){
+			std::cout<<warn<<'\n';
+		}
+		void writeError(std::string_view error){
+			std::cout<<error<<'\n';
+		}
+};
+
+
+class NullLogger : public Logger{
+
+public:
+	void writeInfo(std::string_view info){
+		(void)info;
+	}
+	void writeWarn(std::string_view warn){
+		(void)warn;
+	}
+	void writeError(std::string_view error){
+		(void)error;
+	}
+};
+
+
+
+class LoggerFactory{
+public:
+	enum class loggerType : int{
+		FILE,
+		SOUT,
+		REMOTE,
+	};
+
+	LoggerFactory(std::string_view cfgFileName) : configFileName{cfgFileName}{};
 
 
 
 
-	 std::string readCfg()const{
-		 std::fstream cfgFileStream(configFileName);
-		 std::stringstream ss;
-		 ss << cfgFileStream.rdbuf();
-		 return std::string(ss.str());
-	 }
-
-	 std::string readCfgShort()const{
-			 std::fstream cfgFileStream(configFileName);
-			 return std::string  (std::istreambuf_iterator<char>(cfgFileStream), std::istreambuf_iterator<char>());
-		 }
-
-	 logginOutput evalueateCnfiguration(std::string_view configuration) const{
-
-		 if (configuration.find("SOUT") !=  std::string::npos){
-			 return logginOutput::SOUT;
-		 }
-
-		 if (configuration.find("FILE") !=  std::string::npos){
-			 return logginOutput::FILE;
-		 }
-
-		 return logginOutput::SOUT;
-	 }
 
 
-	 std::shared_ptr<Logger> create () const {
-		 std::string cfgContent = readCfgShort();
-		 logginOutput outputTarget = evalueateCnfiguration(cfgContent);
-		 return createLogger(outputTarget);
-	 }
 
 
-	 std::shared_ptr<Logger> createLogger(logginOutput outputTarget) const {
-		 switch (outputTarget){
-		 case logginOutput::FILE:{
-			 return std::make_shared<FilesystemLogger>();
-			 break;
-		 }
-		 case logginOutput::SOUT:
-		 default: {
-			 return  std::make_shared<StdOutLogger>();
-			 break;
-		 }
-		 }
-	 }
+	std::shared_ptr<Logger> createFromCfg () const {
+		std::string cfgContent = readCfgShort();
+		std::vector<loggerType> loggersToCreate = evalueateCnfiguration(cfgContent);
+		return createLogger(loggersToCreate);
+	}
+
+
+
+
+
+	std::shared_ptr<Logger> createLogger(std::vector<loggerType> typesToCreate) const {
+		std::shared_ptr<CompositLogger> loggerPtr = std::make_shared<CompositLogger>();
+		for (auto &i : typesToCreate){
+			switch (i){
+			case loggerType::FILE:{
+				loggerPtr->addLogger(std::make_shared<FileLogger>());
+				break;
+			}
+			case loggerType::REMOTE:{
+				loggerPtr->addLogger(std::make_shared<RemoteLogger>());
+				break;
+			}
+			case loggerType::SOUT:{
+				loggerPtr->addLogger(std::make_shared<StdOutLogger>());
+				break;
+			}
+			default:{
+				loggerPtr->addLogger( std::make_shared<NullLogger>());
+			}
+			}
+		}
+
+		return std::static_pointer_cast<Logger>(loggerPtr);
+	}
+
+private:
+	std::string configFileName;
+
+
+	std::string readCfg()const;
+
+	std::string readCfgShort()const;
+
+
+	std::vector<loggerType> evalueateCnfiguration(std::string_view configuration) const;
 
 };
+
+
+std::string LoggerFactory::readCfg()const{
+		std::fstream cfgFileStream(configFileName);
+		std::stringstream ss;
+		ss << cfgFileStream.rdbuf();
+		return std::string(ss.str());
+	}
+
+	std::string LoggerFactory::readCfgShort()const{
+		std::fstream cfgFileStream(configFileName);
+		return std::string  (std::istreambuf_iterator<char>(cfgFileStream), std::istreambuf_iterator<char>());
+	}
+
+
+	std::vector<LoggerFactory::loggerType> LoggerFactory::evalueateCnfiguration(std::string_view configuration) const{
+		std::vector<loggerType> usedLoggers;
+		if (configuration.find("SOUT") !=  std::string::npos){
+			usedLoggers.push_back(loggerType::SOUT);
+		}
+
+		if (configuration.find("FILE") !=  std::string::npos){
+			usedLoggers.push_back(loggerType::FILE);
+		}
+
+		if (configuration.find("REMOTE") !=  std::string::npos){
+			usedLoggers.push_back(loggerType::REMOTE);
+		}
+
+		return  usedLoggers;
+	}
+
+
+void fun (std::shared_ptr<int>x){
+		 //*x = 50;
+		 x.reset();
+}
+
+
 
 
 
 int main(){
 	LoggerFactory loggerFactory("logger.cfg");
-	std::shared_ptr<Logger>  logger = loggerFactory.create();
+	std::shared_ptr<Logger>  logger = loggerFactory.createLogger({{LoggerFactory::loggerType::FILE}});
 	logger->writeWarn("here is example warn");
+
+	std::shared_ptr<Logger> log = std::make_shared<FileLogger> ();
+//	log->writeError("here are example errrrror");
+
+
+
+
+
+
+
+	std::shared_ptr<int> intPtr = std::make_shared<int>();
+	*intPtr = 3;
+	*intPtr  = 15;
+	std::shared_ptr<int> x = intPtr;
+
+
+
+	std::cout<< *intPtr;
+	std::cout << std::endl;
+	if (x.unique()){
+		std::cout << "unique";
+	} else{
+		std::cout << "first ref: " << intPtr.use_count();
+	}
+	std::cout << std::endl;
+
+	fun(intPtr);
+
+
+	std::cout<<*intPtr.get();
+	std::cout << std::endl;
+	if (x.unique()){
+		std::cout << "unique";
+	} else{
+		std::cout << "first ref: " << intPtr.use_count();
+	}
+	std::cout << std::endl;
+
+
+	x.reset();
+	if (x){
+	std::cout << std::endl;
+		if (x.unique()){
+			std::cout << "unique";
+		} else{
+			std::cout << " second ref: " << intPtr.use_count();
+		}
+	}
+
+	x = std::make_shared<int>(99);
+
+	if (x){
+	std::cout << std::endl;
+	std::cout << *x;
+
+		if (x.unique()){
+			std::cout << "unique";
+		} else{
+			std::cout << " third ref: " << intPtr.use_count();
+		}
+	}
+
+
 }
