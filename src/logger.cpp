76cdda0f22bea8a252f79b5b764e56c33ed8cdf6
TomaszPluta <tomasz.pluta.g2@gmail.com>
2019-11-23 11:36:04 +0100

    added composite logger

--- src/logger.cpp
+++ src/logger.cpp
@@ -12,30 +12,180 @@
 
 
 
-
-
-
-
-
-
 class Logger
 {
-      std::ostream &_out;
+      std::ostream &out_;
 public:
+    Logger(std::ostream &out) : out_{out}{};
 
     template < typename T >
     friend Logger & operator<< (Logger & logger, T t);
 
-    Logger(std::ostream &out) : _out{out}{};
+    template <typename T>
+    friend Logger & endl (Logger & logger, T in);
 
+    Logger &operator <<(std::ostream& (*os)(std::ostream&))
+     {
+         (*this).out_ << os;
+         return *this;
+     }
 };
+
+
 
 
 template < typename T >
 Logger & operator<< (Logger & logger, T in){
-  logger._out << in;
+  logger.out_ << in;
   return logger;
 }
+
+
+
+
+
+
+
+
+class CompositLogger:public Logger
+{
+  std::vector < std::shared_ptr < Logger >> loggers;
+
+public:
+  void addLogger (std::shared_ptr < Logger > logger)
+  {
+    loggers.push_back (logger);
+  }
+
+ template <typename T>
+ friend CompositLogger& operator<<(CompositLogger &cLogger, T in);
+
+};
+
+
+template <typename T>
+CompositLogger & operator<<(CompositLogger &cLogger, T in){
+	for (auto i : cLogger.loggers){
+		i << in;
+	}
+	return cLogger;
+}
+
+//class LoggerFactory
+//{
+//public:
+//  enum class loggerType:int
+//  {
+//    FILE,
+//    SOUT,
+//    REMOTE,
+//  };
+//
+//    LoggerFactory (std::string_view cfgFileName):configFileName
+//  {
+//  cfgFileName}
+//  {
+//  };
+//
+//  std::shared_ptr < Logger > createFromCfg ()const
+//  {
+//    std::string cfgContent = readCfgShort ();
+//    std::vector < loggerType > loggersToCreate =
+//      evalueateCnfiguration (cfgContent);
+//    return createLogger (loggersToCreate);
+//  }
+//
+//
+//  std::shared_ptr < Logger > createLogger (std::vector < loggerType >
+//					   typesToCreate) const
+//  {
+//    std::shared_ptr < CompositLogger > loggerPtr =
+//      std::make_shared < CompositLogger > ();
+//    for (auto & i:typesToCreate)
+//      {
+//	switch (i)
+//	  {
+//	  case loggerType::FILE:
+//	    {
+//	      loggerPtr->addLogger (std::make_shared < FileLogger > ());
+//	      break;
+//	    }
+//	    case loggerType::REMOTE:
+//	    {
+//	      loggerPtr->addLogger (std::make_shared < RemoteLogger > ());
+//	      break;
+//	    }
+//	  case loggerType::SOUT:
+//	    {
+//	      loggerPtr->addLogger (std::make_shared < StdOutLogger > ());
+//	      break;
+//	    }
+//	  default:
+//	    {
+//	      loggerPtr->addLogger (std::make_shared < NullLogger > ());
+//	    }
+//	  }
+//      }
+//
+//    return std::static_pointer_cast < Logger > (loggerPtr);
+//  }
+//
+//private:
+//  std::string configFileName;
+//
+//
+//  std::string readCfg ()const;
+//
+//  std::string readCfgShort ()const;
+//
+//
+//  std::vector < loggerType >
+//    evalueateCnfiguration (std::string_view configuration) const;
+//
+//};
+//
+//
+//std::string LoggerFactory::readCfg () const
+//{
+//  std::fstream cfgFileStream (configFileName);
+//  std::stringstream ss;
+//  ss << cfgFileStream.rdbuf ();
+//  return std::string (ss.str ());
+//}
+//
+//std::string LoggerFactory::readCfgShort () const
+//{
+//  std::fstream cfgFileStream (configFileName);
+//  return std::string (std::istreambuf_iterator < char >(cfgFileStream),
+//		      std::istreambuf_iterator < char >());
+//}
+//
+//
+//std::vector < LoggerFactory::loggerType >
+//  LoggerFactory::
+//evalueateCnfiguration (std::string_view configuration) const
+//{
+//  std::vector < loggerType > usedLoggers;
+//  if (configuration.find ("SOUT") != std::string::npos)
+//    {
+//      usedLoggers.push_back (loggerType::SOUT);
+//    }
+//
+//  if (configuration.find ("FILE") != std::string::npos)
+//    {
+//      usedLoggers.push_back (loggerType::FILE);
+//    }
+//
+//  if (configuration.find ("REMOTE") != std::string::npos)
+//    {
+//      usedLoggers.push_back (loggerType::REMOTE);
+//    }
+//
+//  return usedLoggers;
+//}
+//
+//
+
 
 
 
@@ -46,13 +196,15 @@ main ()
 
   Logger ll (std::cout);
   ll << "we log everything" << " even the fact that we are logging now for "
-    << 13 << "times\n";
+    << 13 << "times"<<std::endl;
+  ll<<"and new line after endl";
+
+
 
   std::ofstream fileStreamLog("logTest.txt", std::ofstream::app);
   Logger lf (fileStreamLog);
   lf << "there is example of file log" << " we can use it as same as cout "
-    << 99 << "ho ho, so we can see that overloaded endl is what we need\n";
-
+    << 99 << std::endl<<"blah blah, so we can see that overloaded endl is what we need\n";
 
 
 }
