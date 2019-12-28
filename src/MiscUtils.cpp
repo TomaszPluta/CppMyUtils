1e0971cdd21f3fbf32245f98b5d93727b630aab3
TomaszPluta <tomasz.pluta.g2@gmail.com>
2019-12-28 23:11:24 +0100

    added another piece of functionality

--- src/MiscUtils.cpp
+++ src/MiscUtils.cpp
@@ -1,28 +1,14 @@
-#include "mvc.h"
-
-#include "vector"
-#include "queue"
-#include "list"
-
-
+#include "doubleBuff.h"
+#include <iostream>
 
 
 
 
 int main(int argc, char **argv)
 {
-	std::priority_queue<std::pair<int, std::string>> que;
-	que.push(std::pair<int, std::string>(10, "asd"));
-	que.push(std::pair<int, std::string>(8, "yyy"));
-	que.push(std::pair<int, std::string>(15, "zzz"));
-	que.push(std::pair<int, std::string>(75, "kjh"));
-	que.push(std::pair<int, std::string>(65, "poi"));
-
-
-	std::cout<<std::pair<int, std::string>(que.top()).first;
-
-	que.pop();
-//	que.top()<<std::endl;
+	dBuff<int> db(100);
+	db.write(4);
+//	std::cout<<db.read();
 }
 
 
--- src/doubleBuff.h
+++ src/doubleBuff.h
@@ -1,9 +1,41 @@
 #include <vector>
+#include <memory>
+
 
 template <typename T>
 class dBuff{
-	std::vector<T> current;
-	std::vector<T> next;
+//	std::vector<T> buff1;
+//	std::vector<T> buff2;
+	std::unique_ptr<std::vector<T>> currentPtr;
+	std::unique_ptr<std::vector<T>> nextPtr;
+public:
+
+	dBuff(int size) {
+		currentPtr = std::make_unique<std::vector<T>>(size);
+		nextPtr = std::make_unique<std::vector<T>>(size);
+	}
+
+	void write(T data){
+		if (nextPtr->size() > nextPtr->max_size()){
+			swap();
+		}
+		nextPtr->push_back(data);
+	}
+
+	void swap (void){
+		currentPtr.swap(nextPtr);
+	}
+
+//	T read(){
+//		//send to stream
+//	}
+
+
+
+	T operator[](int index){
+		return currentPtr[index];
+	}
+
 
 
 };
