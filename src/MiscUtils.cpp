cd0ee6b43080aa81ea04a388e582b6283a0d4d67
TomaszPluta <tomasz.pluta.g2@gmail.com>
2019-12-30 23:58:34 +0100

    testing doublebuf with threads

--- src/MiscUtils.cpp
+++ src/MiscUtils.cpp
@@ -4,29 +4,11 @@
 
 
 
+
 int main(int argc, char **argv)
 {
-	dBuff<int> db(5);
-	db.write(1);
-	db.write(2);
-	db.write(3);
-	db.write(4);
-	db.write(5);
-	std::cout<<db[0]<<"\n"<<std::endl; /* print read buff value, just after swap*/
-
-	db.write(10);
-	db.write(20);
-	db.write(30);
-	db.write(40);
-	db.write(50);
-	std::cout<<db[0]<<"\n"<<std::endl;
-
-	db.write(100);
-	db.write(200);
-	db.write(300);
-	db.write(400);
-	db.write(500);
-  	std::cout<<db[0]<<"\n"<<std::endl;
+	dBuff<int> dbuff(10);
+	TestDoubleBuffReadWrite(dbuff);
 
 }
 
--- src/doubleBuff.h
+++ src/doubleBuff.h
@@ -4,14 +4,21 @@
 #include <mutex>
 #include <iostream>
 
+#include <thread>
+#include <chrono>
 
 template <typename T>
 class dBuff{
-//	std::vector<T> buff1;
-//	std::vector<T> buff2;
 	std::unique_ptr<std::vector<T>> currentPtr;
 	std::unique_ptr<std::vector<T>> nextPtr;
 	std::mutex mx;
+
+	void swap (void){
+		std::unique_lock<std::mutex>lock(mx);
+		currentPtr.swap(nextPtr);
+		currentPtr->clear();
+	}
+
 public:
 
 	dBuff(int size) {
@@ -30,21 +37,57 @@ public:
 		}
 	}
 
-	void swap (void){
-		std::unique_lock<std::mutex>lock(mx);
-		currentPtr.swap(nextPtr);
-		currentPtr->clear();
-	}
 
 	template <typename U>
 	void  read(U outputIter){
+		std::unique_lock<std::mutex>lock(mx);
 		std::copy(begin(*currentPtr), end(*currentPtr), outputIter);
 	}
 
 	T operator[](int index){
+		std::unique_lock<std::mutex>lock(mx);
 		return (*currentPtr)[index];
 	}
 
-
-
 };
+
+
+
+dBuff<int> dbuff(10);
+
+void writeToDBuff(dBuff<int> & dbuff){
+	for (int j=0; j< 10; j++){
+		for (int i=0; i<10; i++){
+			dbuff.write(i+(j*10));
+			std::this_thread::sleep_for(std::chrono::milliseconds(500));
+		}
+	}
+}
+
+void readFromDBuff(dBuff<int> & dbuff){
+	for (int j=0; j< 10; j++){
+		for (int i=0; i<10; i++){
+			std::cout<<dbuff[i]<<std::endl;
+			std::this_thread::sleep_for(std::chrono::seconds(1));
+		}
+	}
+}
+
+void TestDoubleBuffReadWrite(dBuff<int> & dbuff){
+	std::thread t1(readFromDBuff, std::ref(dbuff));
+	std::thread t2(writeToDBuff, std::ref((dbuff)));
+	t1.join();
+	t2.join();
+
+}
+
+
+
+
+
+
+
+
+
+
+
